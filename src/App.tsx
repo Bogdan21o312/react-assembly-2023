b53861b860e79f736e38cd13db01faab0fc6b7be
Bogdan21o312 <lutskiv2010@gmail.com>
2023-01-23 18:40:30 +0200

    Deleting unnecessary files, codes

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+    return (
+        <div>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi harum, magni nulla possimus quaerat repellendus sint vero voluptas. Eius excepturi exercitationem laborum quibusdam. Blanditiis labore non officia quo ratione voluptate.
+        </div>
+    );
+};
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
-
-export default App
+export default App;
